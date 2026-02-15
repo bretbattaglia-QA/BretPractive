f2813b0e1d957a08be65dee5021c4ddd62485b5a
bretb <bret.battaglia@gmail.com>
2026-02-14 23:27:27 -0500

    First

---
+++ src/test/java/Tests/FirstTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+    @Test
+    public void openGoogle() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
