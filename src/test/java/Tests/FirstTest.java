ef12799797d1a5cef0be904f7cd3c1c299da0bae
bretb <bret.battaglia@gmail.com>
2026-02-20 02:09:21 -0500

    Updating

--- src/test/java/Tests/FirstTest.java
+++ src/test/java/Tests/FirstTest.java
@@ -1,20 +1,64 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class FirstTest {
 
-    @Test
-    public void openGoogle() {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+    }
 
-        driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
+    @Test(priority = 1)
+    public void testABTesting() {
+        driver.findElement(By.linkText("A/B Testing")).click();
+        Assert.assertTrue(
+                driver.findElement(By.tagName("h3")).getText().contains("A/B Test"),
+                "A/B Testing page did not open."
+        );
 
-        driver.quit();
+        // Go back to homepage
+        driver.navigate().back();
+
+        // 5-second wait for future homepage logic
+        sleep(5000);
+    }
+
+    @Test(priority = 2)
+    public void testAddRemoveElements() {
+        driver.findElement(By.linkText("Add/Remove Elements")).click();
+        Assert.assertTrue(
+                driver.getCurrentUrl().contains("add_remove_elements"),
+                "Add/Remove Elements page did not open."
+        );
+
+        // Go back to homepage
+        driver.navigate().back();
+
+        // 5-second wait again
+        sleep(5000);
+    }
+
+    // Helper method for clean sleeps
+    private void sleep(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ignored) {}
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // DO NOT CLOSE until you say so
+        // driver.quit();
     }
 }
